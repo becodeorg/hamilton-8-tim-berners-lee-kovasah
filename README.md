35fbc094bb7b6e251d8209b345f218d65137f80b
Sam Kovacevic <samkovacevic05@gmail.com>
2023-05-24 16:11:32 +0200

    finalversion

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 [![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/beaczqnd)
+
+Ceci est un exercice pour BeCode.
+L'exercice consiste à reproduire une page d'un site web au plus proche possible de la capture d'écran fournie.
---
+++ script.js
@@ -0,0 +1,21 @@
+// const imgTim = document.getElementsByClassName('timImg')
+// console.log("hey")
+// function turnar(params) {
+//     return Math.floor(Math.random() * max);
+// }
+
+// function random_bg_color() {
+//     let x = Math.floor(Math.random() * 256);
+//     let y = Math.floor(Math.random() * 256);
+//     let z = Math.floor(Math.random() * 256);
+//     let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+//  console.log(bgColor);
+//  document.body.style.background = bgColor;
+//     }
+
+    // random_bg_color()
+// function randomize() {
+//     imgTim.addeventlistener("click", random_bg_color)
+// }
+
+// randomize();
